bc375606855f2f2df40b47228b9d1c00ee8b397d
Gil Mendes <gil00mendes@gmail.com>
2017-05-14 22:01:49 +0100

    Implement volume envelope logic

--- src/sound/volume_envelope.rs
+++ src/sound/volume_envelope.rs
@@ -3,28 +3,39 @@ use self::blip_buf::BlipBuf;
 extern crate blip_buf;
 
 pub struct VolumeEnvelope {
-    enabled: bool,
-    blip: BlipBuf
+    period : u8,
+    goes_up : bool,
+    delay : u8,
+    initial_volume : u8,
+    volume : u8,
 }
 
 impl VolumeEnvelope {
     /// create a new VolumeEnvelope instance
-    pub fn new(blip: BlipBuf) -> VolumeEnvelope {
+    pub fn new() -> VolumeEnvelope {
         VolumeEnvelope {
-            enabled: false,
-            blip: blip
+            period: 0,
+            goes_up: false,
+            delay: 0,
+            initial_volume: 0,
+            volume: 0,
         }
-    }
-
-    /// check if the channel are enabled
-    pub fn on(&self) -> bool {
-        self.enabled
     }
 
     /// write a byte on sound channel
     pub fn write_byte(&mut self, address: u16, value: u8) {
         match address {
-            _ => { panic!("TODO VolumeEnvelope write address {:#x}", address) }
+            0x12 | 0x17 | 0x21 => {
+                self.period = value & 0x7;
+                self.goes_up = value & 0x8 == 0x8;
+                self.initial_volume = v >> 4;
+                self.volume = self.initial_volume;
+            },
+            0x14 | 0x19 | 0x23 if value & 0x80 == 0x80 => {
+                self.delay = self.period;
+                self.volume = self.initial_volume;
+            }
+            _ => { panic!("TODO: VolumeEnvelope write address {:#x}", address) }
         }
     }
 }
